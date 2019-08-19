89c6749bca2afe5323ec526dd0b3dfd6a4725702
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-08-19 16:47:21 +0200

    add shiny examples with code and screenshot

--- rshiny.R
+++ rshiny.R
@@ -11,7 +11,7 @@
 #'   bookdown::html_document2:
 #'     theme: simplex
 #'     toc: true
-#'     toc_depth: 2
+#'     toc_depth: 3
 #'     toc_float: 
 #'       collapsed: false
 #'     fig_width: 6.3
@@ -30,7 +30,7 @@ knitr::opts_chunk$set(
   fig.align = "center",
   fig.path = "images/"
 )
-#' 
+
 #' # Introduction
 #' 
 #' Shiny is an R package that makes it easy to build interactive 
@@ -53,19 +53,240 @@ install.packages("shiny")
 if (!require("remotes")) install.packages("remotes")
 remotes::install_github("rstudio/shiny")
 
-#' Let's test `shiny` and look at an example (eleven built-in examples available).
+#' ## Load Shiny
+#+ shiny-load
+library(shiny)
+
+#' ## Built-in examples {.tabset .tabset-pills}
+#' 
+#' A Shiny app is built with two pieces:
+#' 
+#' + a `ui` (*i.e.*, *user interface*), how and where output have to be displayed.
+#' + a `server`, where computations happen.
+#' 
+#+ app, eval = FALSE
+shinyApp(ui = ui, server = server)
+
+#'
+#' Let's test `shiny` and look at some examples (eleven built-in examples available).
+#'  
+#+ example-list, results = "asis"
+cat(paste("+", list.files(system.file("examples", package = "shiny"))), sep = "\n")
+
+#'
+#' ### "hello" {- .tabset}
 #' 
 #+ example-01, eval = FALSE
-library(shiny)
 runExample("01_hello")
 
-#+ example-01-webshot, echo = FALSE, eval = FALSE
+#+ example-01-webshot, echo = FALSE
 webshot::appshot(
   app = system.file("examples", "01_hello", package = "shiny"), 
-  file = "images/01_hello.png"
+  delay = 1,
+  vwidth = 6.3 * 100 * 1.5,
+  vheight = 4.7 * 100 * 1.5,
+  selector = ".container-fluid",
+  envvars = c(display.mode = "normal"),
+  file = "images/01_hello", 
 )
 
-#+ example-01-ui, echo = FALSE
-cat(readLines(system.file("examples", "01_hello", "app.R", package = "shiny")), sep = "\n")
-  
-    
+#' ![](images/01_hello.png)
+#' 
+#' #### ui {-}
+#' 
+#+ example-01-ui, echo = FALSE, comment = ""
+ex_app <- readLines(system.file("examples", "01_hello", "app.R", package = "shiny"))
+ex_app <- ex_app[-c(grep("# *", ex_app), which(nchar(ex_app) == 0))]
+cat(ex_app[grep("^ui <- ", ex_app):grep("^)$", ex_app)[1]], sep = "\n")
+
+#' #### server {-}
+#' 
+#+ example-01-server, echo = FALSE, comment = ""
+cat(ex_app[grep("^server <- ", ex_app):grep("^}$", ex_app)[1]], sep = "\n")
+
+#'
+#' ### "text" {- .tabset}
+#' 
+#+ example-02, eval = FALSE
+runExample("02_text")
+
+#+ example-02-webshot, echo = FALSE
+webshot::appshot(
+  app = system.file("examples", "02_text", package = "shiny"), 
+  delay = 1,
+  vwidth = 6.3 * 100 * 1.5,
+  vheight = 4.7 * 100 * 1.5,
+  selector = ".container-fluid",
+  envvars = c(display.mode = "normal"),
+  file = "images/02_text", 
+)
+
+#' ![](images/02_text.png)
+#' 
+#' #### ui {-}
+#' 
+#+ example-02-ui, echo = FALSE, comment = ""
+ex_app <- readLines(system.file("examples", "02_text", "app.R", package = "shiny"))
+ex_app <- ex_app[-c(grep("# ", ex_app), which(nchar(ex_app) == 0))]
+cat(ex_app[grep("^ui <- ", ex_app):grep("^)$", ex_app)[1]], sep = "\n")
+
+#' #### server {-}
+#' 
+#+ example-02-server, echo = FALSE, comment = ""
+cat(ex_app[grep("^server <- ", ex_app):grep("^}$", ex_app)[1]], sep = "\n")
+
+#'
+#' ### "reactivity" {- .tabset}
+#' 
+#+ example-03, eval = FALSE
+runExample("03_reactivity")
+
+#+ example-03-webshot, echo = FALSE
+webshot::appshot(
+  app = system.file("examples", "03_reactivity", package = "shiny"), 
+  delay = 1,
+  vwidth = 6.3 * 100 * 1.5,
+  vheight = 4.7 * 100 * 1.5,
+  selector = ".container-fluid",
+  envvars = c(display.mode = "normal"),
+  file = "images/03_reactivity", 
+)
+
+#' ![](images/03_reactivity.png)
+#' 
+#' #### ui {-}
+#' 
+#+ example-03-ui, echo = FALSE, comment = ""
+ex_app <- readLines(system.file("examples", "03_reactivity", "app.R", package = "shiny"))
+ex_app <- ex_app[-c(grep("# ", ex_app), which(nchar(ex_app) == 0))]
+cat(ex_app[grep("^ui <- ", ex_app):grep("^)$", ex_app)[1]], sep = "\n")
+
+#' #### server {-}
+#' 
+#+ example-03-server, echo = FALSE, comment = ""
+cat(ex_app[grep("^server <- ", ex_app):grep("^}$", ex_app)[1]], sep = "\n")
+
+#'
+#' ### "mpg" {- .tabset}
+#' 
+#+ example-04, eval = FALSE
+runExample("04_mpg")
+
+#+ example-04-webshot, echo = FALSE
+webshot::appshot(
+  app = system.file("examples", "04_mpg", package = "shiny"), 
+  delay = 1,
+  vwidth = 6.3 * 100 * 1.5,
+  vheight = 4.7 * 100 * 1.5,
+  selector = ".container-fluid",
+  envvars = c(display.mode = "normal"),
+  file = "images/04_mpg", 
+)
+
+#' ![](images/04_mpg.png)
+#' 
+#' #### ui {-}
+#' 
+#+ example-04-ui, echo = FALSE, comment = ""
+ex_app <- readLines(system.file("examples", "04_mpg", "app.R", package = "shiny"))
+ex_app <- ex_app[-c(grep("# ", ex_app), which(nchar(ex_app) == 0))]
+cat(ex_app[grep("^ui <- ", ex_app):grep("^)$", ex_app)[1]], sep = "\n")
+
+#' #### server {-}
+#' 
+#+ example-04-server, echo = FALSE, comment = ""
+cat(ex_app[grep("^server <- ", ex_app):grep("^}$", ex_app)[1]], sep = "\n")
+
+#'
+#' ### "sliders" {- .tabset}
+#' 
+#+ example-05, eval = FALSE
+runExample("05_sliders")
+
+#+ example-05-webshot, echo = FALSE
+webshot::appshot(
+  app = system.file("examples", "05_sliders", package = "shiny"), 
+  delay = 1,
+  vwidth = 6.3 * 100 * 1.5,
+  vheight = 4.7 * 100 * 1.5,
+  selector = ".container-fluid",
+  envvars = c(display.mode = "normal"),
+  file = "images/05_sliders", 
+)
+
+#' ![](images/05_sliders.png)
+#' 
+#' #### ui {-}
+#' 
+#+ example-05-ui, echo = FALSE, comment = ""
+ex_app <- readLines(system.file("examples", "05_sliders", "app.R", package = "shiny"))
+ex_app <- ex_app[-c(grep("# ", ex_app), which(nchar(ex_app) == 0))]
+cat(ex_app[grep("^ui <- ", ex_app):grep("^)$", ex_app)[1]], sep = "\n")
+
+#' #### server {-}
+#' 
+#+ example-05-server, echo = FALSE, comment = ""
+cat(ex_app[grep("^server <- ", ex_app):grep("^}$", ex_app)[1]], sep = "\n")
+
+#'
+#' ### "tabsets" {- .tabset}
+#' 
+#+ example-06, eval = FALSE
+runExample("06_tabsets")
+
+#+ example-06-webshot, echo = FALSE
+webshot::appshot(
+  app = system.file("examples", "06_tabsets", package = "shiny"), 
+  delay = 1,
+  vwidth = 6.3 * 100 * 1.5,
+  vheight = 4.7 * 100 * 1.5,
+  selector = ".container-fluid",
+  envvars = c(display.mode = "normal"),
+  file = "images/06_tabsets", 
+)
+
+#' ![](images/06_tabsets.png)
+#' 
+#' #### ui {-}
+#' 
+#+ example-06-ui, echo = FALSE, comment = ""
+ex_app <- readLines(system.file("examples", "06_tabsets", "app.R", package = "shiny"))
+ex_app <- ex_app[-c(grep("# ", ex_app), which(nchar(ex_app) == 0))]
+cat(ex_app[grep("^ui <- ", ex_app):grep("^)$", ex_app)[1]], sep = "\n")
+
+#' #### server {-}
+#' 
+#+ example-06-server, echo = FALSE, comment = ""
+cat(ex_app[grep("^server <- ", ex_app):grep("^}$", ex_app)[1]], sep = "\n")
+
+#'
+#' ### "widgets" {- .tabset}
+#' 
+#+ example-07, eval = FALSE
+runExample("07_widgets")
+
+#+ example-07-webshot, echo = FALSE
+webshot::appshot(
+  app = system.file("examples", "07_widgets", package = "shiny"), 
+  delay = 1,
+  vwidth = 6.3 * 100 * 1.5,
+  vheight = 4.7 * 100 * 1.5,
+  selector = ".container-fluid",
+  envvars = c(display.mode = "normal"),
+  file = "images/07_widgets", 
+)
+
+#' ![](images/07_widgets.png)
+#' 
+#' #### ui {-}
+#' 
+#+ example-07-ui, echo = FALSE, comment = ""
+ex_app <- readLines(system.file("examples", "07_widgets", "app.R", package = "shiny"))
+ex_app <- ex_app[-c(grep("# ", ex_app), which(nchar(ex_app) == 0))]
+cat(ex_app[grep("^ui <- ", ex_app):grep("^)$", ex_app)[1]], sep = "\n")
+
+#' #### server {-}
+#' 
+#+ example-07-server, echo = FALSE, comment = ""
+cat(ex_app[grep("^server <- ", ex_app):grep("^}$", ex_app)[1]], sep = "\n")
+
