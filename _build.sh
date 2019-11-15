40f9fa9c254890ce83e9d05078e9dc051df0eb4d
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-11-15 12:11:56 +0100

    add missing packages

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-Rscript -e "rmarkdown::render('rshiny.R',  encoding = 'UTF-8')" \
+Rscript -e "rmarkdown::render('rshiny.Rmd',  encoding = 'UTF-8')" \
   && head -n -6 rshiny.md | tail -n +30 > README.md \
   && rm rshiny.md
