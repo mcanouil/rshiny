f36f5302de2ba2f31200ed35b557e568e4d0ac83
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-11-18 16:23:33 +0100

    add progress and notification; add page document

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-Rscript -e "rmarkdown::render('rshiny.Rmd',  encoding = 'UTF-8')" \
-  && head -n -6 rshiny.md | tail -n +30 > README.md \
-  && rm rshiny.md
+Rscript -e "rmarkdown::render('rshiny.Rmd', output_file = 'page.html', output_format = 'html_document',  encoding = 'UTF-8')"
+
+Rscript -e "rmarkdown::render('rshiny.Rmd', output_format = 'html_document',  encoding = 'UTF-8')"
---
+++ materials/35-app/app.R
@@ -0,0 +1,21 @@
+library("shiny")
+
+ui <- fluidPage(
+  plotOutput("plot", height = "300px")
+)
+
+server <- function(input, output, session) {
+  output$plot <- renderPlot({
+    ###<b>
+    withProgress(message = "importation des données ...", {
+      for (i in seq_len(10)) {
+        Sys.sleep(0.2)
+        setProgress(i / 10, message = NULL)
+      }
+    })
+    ###</b>
+    plot(1:10)
+  })
+}
+
+shinyApp(ui, server)
