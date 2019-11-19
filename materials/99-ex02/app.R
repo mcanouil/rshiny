5655ba5f657132061ccb81b083cbf20643c7baa3
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2019-11-19 18:17:40 +0100

    add exercises for input/output/layout

---
+++ materials/99-ex02/app.R
@@ -0,0 +1,31 @@
+library("shiny")
+
+ui <- fluidPage(
+  textInput("lastname", "Nom", value = "Nom"),
+  textInput("firstname", "Prénom", value = "Prénom"),
+  ###<b>
+  textOutput("greetings"),
+  ###</b>
+  numericInput("age", "Age", value = 0),
+  radioButtons("sex", "Sexe", 
+    choices = c("Femme", "Homme", "Ne sait pas"), 
+    selected = "Ne se prononce pas"
+  ),
+  checkboxGroupInput("hobbies", "Loisirs", 
+    choices = c("Vélo", "Rando", "Natation", "Badminton", "Karaté", "Autres")
+  )
+)
+
+server <- function(input, output, session) { 
+  ###<b>
+  output$greetings <- renderText({ 
+    sample(c(
+    "Bonjour !", "Bienvenue !", "Hello !", 
+    "Salutations !", "Comment allez-vous ?",
+    "Heu, vous êtes qui ?"
+    ), 1)
+  })
+  ###</b>
+}
+
+shinyApp(ui, server)
