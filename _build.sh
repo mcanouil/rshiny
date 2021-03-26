089f828b0e8e376b626b1ddba1efd0273bd76750
Mickaël Canouil <mickael.canouil@cnrs.fr>
2021-03-26 20:02:00 +0100

    switch to xaringam to part3

--- _build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-Rscript -e "rmarkdown::render('rshiny.Rmd', output_file = 'page.html', output_format = 'html_document',  encoding = 'UTF-8')"
-
-Rscript -e "rmarkdown::render('rshiny.Rmd', output_format = 'html_document',  encoding = 'UTF-8')"
